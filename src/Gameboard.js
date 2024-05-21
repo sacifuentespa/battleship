16be5ca3e495c8f7c32dec30ce36328b1d703d21
sacifuentespa <85040512+sacifuentespa@users.noreply.github.com>
2024-05-20 23:26:26 -0500

    added names to the ships

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,5 +1,5 @@
-class Gameboard{
-    constructor(){
+class Gameboard {
+    constructor() {
         this.GRID_SIZE = 10;
         this.grid = this.createGrid();
     }
@@ -7,10 +7,56 @@ class Gameboard{
     createGrid() {
         const grid = [];
         for (let i = 0; i < this.GRID_SIZE; i++) {
-          grid.push(Array(this.GRID_SIZE).fill(null));
+            grid.push(Array(this.GRID_SIZE).fill(null));
         }
         return grid;
-      }
+    }
 
-      
+    isValidPlacement(ship, row, col, horizontal) {
+        if (horizontal && col + ship.length > this.gridSize) {
+            return false;
+        }
+        if (!horizontal && row + ship.length > this.gridSize) {
+            return false;
+        }
+
+        for (let i = 0; i < ship.length; i++) {
+            const currentRow = horizontal ? row : row + i;
+            const currentCol = horizontal ? col + i : col;
+            if (this.grid[currentRow][currentCol] !== null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // horizontal being a boolean value true for horizontal, false for vertical
+    
+    placeShip(ship, row, col, horizontal) {
+        if (!this.isValidPlacement(ship, row, col, horizontal)) {
+            return false;
+        }
+
+        const shipCells = [];
+        for (let i = 0; i < ship.length; i++) {
+            if (horizontal) {
+                this.grid[row][col + i] = ship;
+                shipCells.push({ row, col: col + i });
+            } else {
+                this.grid[row + i][col] = ship;
+                shipCells.push({ row: row + i, col });
+            }
+        }
+        
+        // check placement
+        ship.place(shipCells);
+        this.ships.push(ship);
+        return true;
+    }
+
 }
+
+let gridTest = new Gameboard;
+
+console.log(gridTest.grid);
--- src/Ship.js
+++ src/Ship.js
@@ -1,9 +1,22 @@
 class Ship {
-    constructor(length){
+    constructor(length, name){
         if (!Number.isInteger(length) || length <= 0) {
             throw new Error('Length must be a positive integer.');
           }
         this.length = length;
+        switch(this.length){
+            case 5:
+                this.name = "Carrier";
+                break;
+            case 4:
+                this.name = "Battleship";
+                break;
+            case 3:
+                this.name = "Destroyer";
+                break;
+            default:
+                this.name = "Patrol Boat";
+        }    
         this.hits = 0;
     }
 
@@ -18,8 +31,5 @@ class Ship {
     }
 
 }
-
-
-console.log(ship1)
 
 export default Ship;
